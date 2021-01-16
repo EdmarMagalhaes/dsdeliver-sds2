540d2acbe0a14846ebe6efc5f694db99310c4731
EdmarMagalhaes <edmarpmc@gmail.com>
2021-01-16 13:18:21 -0300

    Fix bugs Backend

--- Backend/src/main/java/com/devsuperior/dsdeliver/dto/OrderDTO.java
+++ Backend/src/main/java/com/devsuperior/dsdeliver/dto/OrderDTO.java
@@ -43,7 +43,7 @@ public class OrderDTO implements Serializable {
 		id = entity.getId();
 		address = entity.getAddress();
 		latitude = entity.getLatitude();
-		longitude = entity.getLatitude();
+		longitude = entity.getLongitude();
 		moment = entity.getMoment();
 		status = entity.getStatus();
 		total = entity.getTotal();
